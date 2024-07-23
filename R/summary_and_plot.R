c29cb9d94e5698e25db7da61fea183f1725ff3fe
Emma Sofie Skarstein <emma@skarstein.no>
2024-07-23 14:54:33 +0200

    add option for correct model in plot

--- R/summary_and_plot.R
+++ R/summary_and_plot.R
@@ -90,6 +90,7 @@ plot_inlamisclass <- function(mcmc_results, niter = length(mcmc_results),
 #' @inheritParams make_results_df
 #' @param mcmc_results an inlamisclass model or list of inlamisclass models.
 #' @param naive_mod a model that does not account for misclassification
+#' @param correct_mod a model that uses the true values of the covariate with misclassification (only possible in simulation studies)
 #' @param plot_intercept should estimated intercept be plotted?
 #' @param num_inlamisclass_models number of inlamisclass models if mcmc_results is a list
 #'
@@ -97,8 +98,10 @@ plot_inlamisclass <- function(mcmc_results, niter = length(mcmc_results),
 #' @export
 #' @importFrom ggplot2 ggplot aes vars
 #' @importFrom rlang .data
-plot_compare_inlamisclass <- function(mcmc_results, niter = NULL,
-                              nburnin = 0, naive_mod, plot_intercept = FALSE, num_inlamisclass_models = 1){
+plot_compare_inlamisclass <- function(mcmc_results, naive_mod, correct_mod = NULL,
+                                      niter = NULL, nburnin = 0,
+                                      plot_intercept = FALSE,
+                                      num_inlamisclass_models = 1){
 
   if(is.null(niter)){
     niter <- length(mcmc_results)
@@ -106,23 +109,47 @@ plot_compare_inlamisclass <- function(mcmc_results, niter = NULL,
   if(num_inlamisclass_models == 1){
     mcmc_results <- list(mcmc_results)
   }
+  # Summary of misclassification model(s)
   summary_list <- list()
   for(i in 1:num_inlamisclass_models){
-    summary_list[[i]] <- make_results_df(mcmc_results[[i]], niter = niter, nburnin = nburnin)$moi
+    summary_list[[i]] <- make_results_df(mcmc_results = mcmc_results[[i]],
+                                         niter = niter, nburnin = nburnin)$moi
   }
   summary_inlamisclass <- dplyr::bind_rows(summary_list, .id = "submodel")
+
+  # Summary of naive model
   summary_naive <- naive_mod$summary.fixed
   summary_naive$variable <- rownames(summary_naive)
 
-  if(!plot_intercept){
-    summary_inlamisclass <- dplyr::filter(summary_inlamisclass, .data$variable != "(Intercept)")
-    summary_naive <- dplyr::filter(summary_naive, .data$variable != "(Intercept)")
+  # Summary of correct model
+  if(!is.null(correct_mod)){
+    summary_correct <- correct_mod$summary.fixed
+    summary_correct$variable <- rownames(summary_correct)
+
+    w_name <- setdiff(summary_naive$variable, summary_correct$variable)
+    x_name <- setdiff(summary_correct$variable, summary_naive$variable)
+
+    # Rename to correct coef name (so they are all beta_x)
+    summary_inlamisclass[which(summary_inlamisclass$variable == w_name), "variable"] <- x_name
+    summary_naive[which(summary_naive$variable == w_name), "variable"] <- x_name
   }
 
   if(num_inlamisclass_models == 1){summary_inlamisclass$submodel <- rep("", nrow(summary_inlamisclass))}
   summary_naive$submodel <- ""
 
-  all_summary <- dplyr::bind_rows(inlamisclass = summary_inlamisclass, naive = summary_naive, .id = "model_type")
+
+  if(is.null(correct_mod)){
+    all_summary <- dplyr::bind_rows(inlamisclass = summary_inlamisclass,
+                                    naive = summary_naive, .id = "model_type")
+  }else{
+    all_summary <- dplyr::bind_rows(inlamisclass = summary_inlamisclass,
+                                    naive = summary_naive,
+                                    correct = summary_correct, .id = "model_type")
+  }
+
+  if(!plot_intercept){
+    all_summary <- dplyr::filter(all_summary, .data$variable != "(Intercept)")
+  }
 
   all_summary$model <- paste0(all_summary$model_type, "[", all_summary$submodel, "]")
 
